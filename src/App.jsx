a1c65fec23138f50b1b68e06f6fae042cefa5336
Ghayoor Abbas <ghayoormastoi1531@gmail.com>
2026-07-23 08:35:19 -0700

    Add Footer and Services with correct case

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer123";
+import Footer from "./components/Footer";
 
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
-import Services from "./sections/Services123";
+import Services from "./sections/Services";
 import Contact from "./sections/Contact";
 
 import Loader from "./components/Loader";
