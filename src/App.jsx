ab6f2f7bafb6a782af606f706447aed713218cf4
Ghayoor Abbas <ghayoormastoi1531@gmail.com>
2026-07-23 08:32:13 -0700

    Rename files temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Footer from "./components/Footer123";
 
 import Hero from "./sections/Hero";
 import About from "./sections/About";
 import Skills from "./sections/Skills";
 import Projects from "./sections/Projects";
-import Services from "./sections/Services";
+import Services from "./sections/Services123";
 import Contact from "./sections/Contact";
 
 import Loader from "./components/Loader";
